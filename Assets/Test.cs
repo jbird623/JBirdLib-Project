333a5af5b6ef37c67b1f066dc3fec8e71098b690
Justin JBird Cirigliano <jcirigliano623@gmail.com>
2022-08-14 04:32:59 -0400

    Creating test script and updates to JBirdLib

---
+++ Assets/Test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public JBirdLib.AngleVector3 angleVector;
+    public Vector3 test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
