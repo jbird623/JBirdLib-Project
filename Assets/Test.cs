4aa2884dd6884f29cec783972af8a0cf6fca885e
Justin JBird Cirigliano <jcirigliano623@gmail.com>
2022-08-16 03:06:24 -0400

    Colors, DataTracking, and a few other small things

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using JBirdLib;
 
 public class Test : MonoBehaviour
 {
-    public JBirdLib.AngleVector3 angleVector2;
-    public Vector3 test;
-    public JBirdLib.LootChance chance;
+    
+    public AngleVector3 angleVector2;
+    public Angle testAngle;
+    public LootChance chance;
 
     // Start is called before the first frame update
     void Start()
